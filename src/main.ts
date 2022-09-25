b8775b52e198259710f39f41cb5bcdace3aa1421
TmTanky <tmtanky@gmail.com>
2022-09-25 10:28:59 +0800

    refactor: logic change when clicking the same card
    feat/generate-cards

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,20 @@ const shuffleCards = (arr: string[]) => {
 }
 
 console.log(shuffleCards(matchers))
+console.log(shuffleCards(matchers))
+console.log(shuffleCards(matchers))
+console.log(shuffleCards(matchers))
 
 const createCards = () => {
-  for (let x = 0; x < shuffleCards(matchers).length; x++) {
+  shuffleCards(matchers).forEach((item, index) => {
     const card = document.createElement('button')
-    console.log(matchers[x])
 
     cardsContainer.appendChild(card)
     card.classList.add('card')
-    card.innerText = matchers[x]
-    card.value = matchers[x]
-
-    // card.addEventListener('click', () => {
-    // })
-  }
+    card.innerText = item
+    card.value = item
+    card.id = `${item}${index}`
+  })
 }
 
 createCards()
@@ -30,28 +30,36 @@ const cards = document.querySelectorAll('.card')
 let pickCounter = 0
 let firstPick: string
 let secondPick: string
+let firstPickId: string
+let secondPickId: string
 
 cards.forEach((item) => {
   item.addEventListener('click', (e) => {
     const event = e.target as HTMLButtonElement
 
     pickCounter = pickCounter + 1
-    console.log(pickCounter)
-
+    
+    
     if (pickCounter === 1) {
       firstPick = event.value as string
+      firstPickId = event.id
     }
-
+    
     if (pickCounter === 2) {
       secondPick = event.value as string
+      secondPickId = event.id
     }
 
-    if (firstPick === secondPick) {
+    if (firstPick === secondPick && firstPickId !== secondPickId) {
       alert('score')
     }
 
     if (pickCounter === 2) {
       pickCounter = 0
+      firstPick = ''
+      secondPick = ''
+      firstPickId = ''
+      secondPickId = ''
     }
   })
 })
