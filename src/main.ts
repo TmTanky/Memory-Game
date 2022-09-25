3b85920f5e3e3c60bb18313aab0838a62c940476
TmTanky <tmtanky@gmail.com>
2022-09-25 14:17:24 +0800

    feat: added removing if cards selected are correct
    feat/improving-game-logic

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const cardsContainer = document.querySelector('.cards') as HTMLDivElement
+const rootCardsContainer = document.querySelector('.cards') as HTMLDivElement
 
 const matchers = ['heart', 'heart', 'diamond', 'diamond', 'circle', 'circle']
 
@@ -6,20 +6,23 @@ const shuffleCards = (arr: string[]) => {
   return arr.sort(() => 0.5 - Math.random())
 }
 
-console.log(shuffleCards(matchers))
-console.log(shuffleCards(matchers))
-console.log(shuffleCards(matchers))
-console.log(shuffleCards(matchers))
-
 const createCards = () => {
   shuffleCards(matchers).forEach((item, index) => {
-    const card = document.createElement('button')
+    const cardContainer = document.createElement('button')
+    const front = document.createElement('div')
+    const back = document.createElement('div')
 
-    cardsContainer.appendChild(card)
-    card.classList.add('card')
-    card.innerText = item
-    card.value = item
-    card.id = `${item}${index}`
+    cardContainer.appendChild(front)
+    cardContainer.appendChild(back)
+
+    front.classList.add('front-card')
+    front.innerText = item
+    back.classList.add('back-card')
+    rootCardsContainer.appendChild(cardContainer)
+    cardContainer.classList.add('card')
+    front.innerText = item
+    cardContainer.value = item
+    cardContainer.id = `${item}${index}`
   })
 }
 
@@ -36,9 +39,9 @@ let secondPickId: string
 cards.forEach((item) => {
   item.addEventListener('click', (e) => {
     const event = e.target as HTMLButtonElement
+    // item.classList.add('show')
 
     pickCounter = pickCounter + 1
-    
     
     if (pickCounter === 1) {
       firstPick = event.value as string
@@ -51,7 +54,9 @@ cards.forEach((item) => {
     }
 
     if (firstPick === secondPick && firstPickId !== secondPickId) {
-      alert('score')
+      document.querySelector(`#${firstPickId}`)?.remove()
+      document.querySelector(`#${secondPickId}`)?.remove()
+      // alert('score')
     }
 
     if (pickCounter === 2) {
